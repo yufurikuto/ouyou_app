b1bd3dfe98a039718c36331e442a8d0f2508ed5e
Rikuto Yufu(油布 陸叶) <149981079+yufurikuto@users.noreply.github.com>
2025-09-29 06:46:14 +0000

    a

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# --- このスクリプトは、コンテナ作成後に一度だけ実行されます ---
+echo "--- Running post-create-command.sh ---"
+
+# --- 実行したいコマンドを追記していきます ---
+
+
+# --- スクリプト終了 ---
+echo "--- Setup completed successfully! ---"
